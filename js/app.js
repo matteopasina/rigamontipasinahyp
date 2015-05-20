403967d03afaf89566cec4179a54be379eef7e72
AliceRi <ali.rigamonti@gmail.com>
2015-05-20 18:04:23 +0200

    fatto dei tentativi/rinominato dei file ma fondamentalmente non ho fatto niente

--- js/app.js
+++ js/app.js
@@ -1,5 +1,10 @@
-angular.module('bigGymApp', [])
-.controller('headerController', function($scope){
-    
-    
+var app = angular.module('bigGymApp', []);
+
+app.controller('headerController', function($scope){
+    $scope.move = {
+    icon: 'img/move.jpg',
+    title: 'MOVE',
+    developer: 'MOVE, Inc.',
+    price: 0.99
+  };
 });
--- js/directives/layout.js
+++
@@ -1,9 +0,0 @@
-app.directive('layout', function() { 
-  return { 
-    restrict: 'E', 
-    scope: { 
-        info: '=' 
-    }, 
-    templateUrl: 'js/directives/layout.html' 
-  }; 
-})
