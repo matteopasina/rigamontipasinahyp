1fcf1e4bbbed452bda0ebeb393eb97a4dbd70ade
matteopasina <matteopasina@gmail.com>
2015-05-22 14:08:07 +0200

    instructor page

--- js/instructors.js
+++ js/instructors.js
@@ -1,5 +1,5 @@
 var instructors = angular.module('allinstructors', []);
 app.controller('instructorsCtrl', function($scope, $http) {
-    $http.get("http://grandepalestra.altervista.org/Connessione.php")
+    $http.get("http://grandepalestra.altervista.org/Instructors.php")
     .success(function(response) {$scope.instructors = response;});
 });
