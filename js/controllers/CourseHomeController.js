0af99ed3a7395d81b4a94962fec73cb9de0e662e
matteopasina <matteopasina@gmail.com>
2015-05-27 23:35:25 +0200

    navbar messa a posto con il routing, ho appena letto un articolo che sconsigliava di usare le views ma di usare le directives

--- js/controllers/CourseHomeController.js
+++ js/controllers/CourseHomeController.js
@@ -2,54 +2,16 @@ app.controller('CourseHomeController', ['$scope', 'courses', function($scope, co
   courses.success(function(data) {
     $scope.courses = data;
   });
-  $scope.customers = [
-  {
-    name: 'Course By Level',
-    address: 'Corsi.html#/courseByLevel/'
-  },
-  {
-    name: 'Courses By Category',
-    address: 'Corsi.html#/coursesByCategory/'
-  }
-];
-}])
-.directive('myCustomer', function() {
-  return {
-    scope: { 
-      info: '=' 
-    }, 
-    templateUrl: 'header.html'
-  };
-});
+}]);
 
 app.controller('CourseLevelController', ['$scope', 'coursesByLevel', function($scope, coursesByLevel) {
   coursesByLevel.success(function(data) {
     $scope.courses = data;
   });
-  $scope.customers = [
-  {
-    name: 'Courses',
-    address: 'Corsi.html#/'
-  },
-  {
-    name: 'Courses by Category',
-    address: 'Corsi.html#/coursesByCategory/'
-  }
-];
 }])
 app.controller('CourseByCourseCategoryController', ['$scope', 'coursesByCategory', function($scope, coursesByCategory) {
   coursesByCategory.success(function(data) {
     $scope.courses = data;
   });
-  $scope.customers = [
-  {
-    name: 'Courses',
-    address: 'Corsi.html#/'
-  },
-  {
-    name: 'Courses by Level',
-    address: 'Corsi.html#/courseByLevel/'
-  }
-];
 }])
 
--- js/controllers/NavbarController.js
+++ js/controllers/NavbarController.js
@@ -2,21 +2,49 @@ app.controller('Controller', ['$scope', function($scope) {
   $scope.customers = [
   {
     name: 'Instructors of the month',
-    address: 'location.html'
+    address: '#/Location'
   },
   {
     name: 'Courses',
-    address: 'location.html'
+    address: '#/CourseHome'
   }
 ];
   $scope.location = [
   {
     name: 'ProvaLocation1',
-    address: 'location.html'
+    address: '#/Location'
   },
   {
     name: 'ProvaLocation2',
-    address: 'location.html'
+    address: '#/Location'
+  }
+ ];
+  $scope.courseHome = [
+  {
+    name: 'Course By Level',
+    address: 'index.html#/courseByLevel'
+  },
+  {
+    name: 'Courses By Category',
+    address: 'index.html#/coursesByCategory'
+  }];
+      $scope.courseLevel = [
+  {
+    name: 'Courses',
+    address: 'index.html#/CourseHome'
+  },
+  {
+    name: 'Courses by Category',
+    address: 'index.html#/coursesByCategory'
+  }];
+      $scope.courseCategory = [
+  {
+    name: 'Courses',
+    address: 'index.html#/CourseHome'
+  },
+  {
+    name: 'Courses by Level',
+    address: 'index.html#/courseByLevel'
   }
  ]
 }])
