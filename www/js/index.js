460e0393c0030b3cd59dd64639b7859d3351e912
AliceRi <ali.rigamonti@gmail.com>
2015-06-09 18:38:21 +0200

    prova phonegap

---
+++ www/js/index.js
@@ -0,0 +1,14 @@
+var appInit = {
+    initialize: function() {
+        this.bindEvents();
+    },
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, true);
+    },
+
+    onDeviceReady: function() {
+        angular.element(document).ready(function() {
+            angular.bootstrap(document);
+        });
+    },
+};
